e2aa8c264c71c93d706eefa536a7beb1c89500fb
Rosa-Petursdottir <54938845+Rosa-Petursdottir@users.noreply.github.com>
2019-12-02 14:21:36 +0000

    hu

--- verklegt_1_main.py
+++ verklegt_1_main.py
@@ -1,4 +1,20 @@
-test
+def open_file(filename) :
+    file_object = open(filename, "r")
+    return file_object
 
-test2
-drills only
+def make_crew_dict(file_object) :
+    crew_dict= {}
+    for line in file_object :
+        ssn,name,role,rank,licence = line.strip().split(",")
+        
+        key = name
+        crew_dict[key] = (int(ssn), role, rank, licence)
+    return crew_dict
+        
+def main() :
+    filename = "crew.csv"
+    file_object = open_file(filename)
+    crew_dict = make_crew_dict(file_object)
+    print(crew_dict)
+
+main()
