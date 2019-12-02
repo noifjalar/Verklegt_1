3c55b256adc21606c5218b4fff1c7cc26557401f
magnusthorm <mmagnusthor@gmail.com>
2019-12-02 17:19:40 +0000

    drillur frákongnum

--- Aircraft.py
+++ Aircraft.py
@@ -3,5 +3,5 @@ import csv
 with open('Aircraft.csv', newline= '') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        print(row)
+        print(row[])
         # print(row['planeTypeId'], row['planeType'])
--- AircraftType.py
+++ AircraftType.py
@@ -1,8 +1,18 @@
 import csv
 
+list = []
 with open('AircraftType.csv', newline= '') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        print(row)
-        # print(row['planeTypeId'], row['planeType'])
-        
+        atm = AircraftTypeModel()
+        atm.planeTypeId = row["planeTypeId"]
+        list.append(atm)
+        # print(row)
+        #for key in row:
+        #    print(str(key) + ": " + str(row[key]), end=" ")
+        #print(row['planeTypeId'], row['manufacturer'])
+
+class AircraftTypeModel:
+    def __init__(self):
+        self.planeTypeId = ""
+        self.manufacturer = ""
