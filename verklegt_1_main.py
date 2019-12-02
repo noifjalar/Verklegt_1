9e1acaf5a4bb780c8375c344032708a5c5ececf7
Rosa-Petursdottir <54938845+Rosa-Petursdottir@users.noreply.github.com>
2019-12-02 15:35:52 +0000

    Update verklegt_1_main.py

--- verklegt_1_main.py
+++ verklegt_1_main.py
@@ -1,12 +1,20 @@
 def print_main_menu():
+    print("\tMain Menu")
+    print("")
     print("Shift manager:")
-    print("\t") #1
-    print("\t") #2
-    print("\t") #3
-    print("\t") #4
-    
+    print("\t(1) - Register employee") #1
+    print("\t(2) - Change employee info") #2
+    print("\t(3) - Assign cabin/pilot to voyage") #3
+    print("\t(4) - Display voyage") #4
+    print("")
     print("Registration manager:")
-    print("\t") #5
-    print("\t") #6
-    print("\t") #7
+    print("\t(5) - Register destinations") #5
+    print("\t(6) - Register airplanes") #6
+    print("\t(7) - Create voyage") #7
+    choice = input("Select an operation with a corresponding number: ")
+    return choice
 
+def main() :
+    choice = print_main_menu()
+
+main()
