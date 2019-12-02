2124726c811b541d8913ecfa7ccf3730a3c85d85
noifjalar <noi_fjalar@hotmail.com>
2019-12-02 15:26:55 +0000

    menu-work

---
+++ verklegt_1_main.py
@@ -0,0 +1,12 @@
+def print_main_menu():
+    print("Shift manager:")
+    print("\t") #1
+    print("\t") #2
+    print("\t") #3
+    print("\t") #4
+    
+    print("Registration manager:")
+    print("\t") #5
+    print("\t") #6
+    print("\t") #7
+
