b2691c7501917b62a7d4594d8d12e5c5c0b22277
Rosa-Petursdottir <54938845+Rosa-Petursdottir@users.noreply.github.com>
2019-12-03 15:58:35 +0000

    Update data.py

--- data.py
+++ data.py
@@ -5,28 +5,34 @@ def open_file(filename) :
     except FileNotFoundError :
         return None
 
-class Employee:
-    def __init__(self, name, ssn):
-        self.name = name
+class EmployeeIO:
+    def __init__(self, ssn, name, role, rank, licence, address, phonenumber):
         self.ssn = ssn
-    
+        self.name = name
+        self.role = role 
+        self.rank = rank
+        self.licence = licence
+        self.address = address
+        self.phonenumber = phonenumber
+
     def __str__(self):
-        return f"Name: {self.name} - SSN: {self.ssn}"
+        return f"Name: {self.name} - SSN: {self.ssn} - role: {self.role} - licence: {self.licence} - address: {self.address} - phonenumber: {self.phonenumber}"
 
     def write_self_to_csv(path_to_csv):
         with(open(path_to_csv, "a")) as csv_file:
             csv_file.write()
+#           return ?????
 
 class CrewIO():
     def __init__(self, file_stream = None) :
-        self.crew = {}
+        self.crew = []
         if file_stream :
             self.__read_data(file_stream)
         
     def __str__(self) :
         result_str = ''
-        for key, value in self.crew.items():
-            result_str += "{}: {}\n".format(key, value)
+        for emp in self.crew.items():
+            result_str += "{}: {}\n".format(self.crew[0], self.crew[1:])
         return result_str
 
     def __read_data(self, file_object) :
@@ -36,18 +42,13 @@ class CrewIO():
                 header = False
                 continue
             else: 
-                ssn,name,role,rank,licence,address,phonenumber = line.strip().split(",")
-                emp = Employee(name, ssn)
-                key = name
-                self.crew[key] = (int(ssn), role, rank, licence, address, int(phonenumber))
-                self.crew[emp]
-        return self.crew
-
+                ssn, name, role, rank, licence, address, phonenumber = line.strip().split(",")
+                emp = EmployeeIO(ssn, name, role, rank, licence, address, phonenumber)
+        return self.crew[emp]
 
 filename = "Crew.csv"    
 file_stream = open_file(filename)
-crew_dict = CrewIO(file_stream)
-print(crew_dict)
-
+crew = CrewIO(file_stream)
+print(crew)
 
 
