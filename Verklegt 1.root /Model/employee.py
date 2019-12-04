663614f6bafdf33b99bb1e93cc2d6426caeb3df3
Rosa-Petursdottir <54938845+Rosa-Petursdottir@users.noreply.github.com>
2019-12-04 14:44:03 +0000

    u

---
+++ Verklegt 1.root /Model/employee.py
@@ -0,0 +1,12 @@
+class Employee :
+    def __init__(self, ssn, name, role, rank, licence, address, phonenumber):
+        self.ssn = ssn
+        self.name = name
+        self.role = role 
+        self.rank = rank
+        self.licence = licence
+        self.address = address
+        self.phonenumber = phonenumber
+
+    def __str__(self):
+        return "Name: {self.name} - SSN: {self.ssn} - role: {self.role} - licence: {self.licence} - address: {self.address} - phonenumber: {self.phonenumber} "
